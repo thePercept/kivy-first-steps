01b0033982a7482c79b08656211eca5737024a72
Abhishek <dev.bizzinnostrategist@gmail.com>
2018-07-21 15:08:04 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+
+    def build(self):
+        return Button(text="PUSH ME !!")
+
+if __name__=='__main__':
+    MyApp().run()
+    
