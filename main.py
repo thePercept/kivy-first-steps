8c81afb9248f725dba691d9ee8fa51dbe5e8d2a4
Abhishek <dev.bizzinnostrategist@gmail.com>
2018-07-21 15:13:22 +0530

    2nd step

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.lang import Builder
+
 
 class MyApp(App):
 
     def build(self):
-        return Button(text="PUSH ME !!")
+         return Builder.load_string('''
+Button
+    text: 'Hello' if self.state == 'normal' else 'World'
+''')
+
+
+
 
 if __name__=='__main__':
     MyApp().run()
-    
